4675657734fc959fc0ce897a6430254ce58efc93
Gwangmu Lee <gwangmu.lee@cscs.ch>
2024-07-31 16:55:03 +0200

    Testing CI/CD...

--- install_dep.sh
+++ install_dep.sh
@@ -5,8 +5,8 @@ LIBSARUS_ROOT_PATH=$(dirname $(realpath "${BASH_SOURCE[0]}"))
 export PATH=$LIBSARUS_ROOT_PATH/spack/spack/bin:$PATH
 . $LIBSARUS_ROOT_PATH/spack/spack/share/spack/setup-env.sh
 
-spack mirror add v0.22.1 https://binaries.spack.io/v0.22.1
 spack buildcache keys --install --trust
+spack bootstrap trust
 spack env activate spack
 spack install
 
