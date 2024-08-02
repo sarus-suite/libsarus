126adabc8ab83acb7a46e9279fe9814fd2a45e6a
Gwangmu Lee <gwangmu.lee@cscs.ch>
2024-08-02 13:31:11 +0200

    Testing CI/CD...

--- build.sh
+++ build.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 LIBSARUS_ROOT_PATH=$(dirname $(realpath "${BASH_SOURCE[0]}"))
+LOCAL_SPACK_PATH=$LIBSARUS_ROOT_PATH/spack/spack/bin
 
 BUILD_DIR=$PWD/build
 TOOLCHAIN_FILE=gcc.cmake
 BUILD_TYPE=Debug
 INSTALL_DIR=$PWD/build/install
 
-export PATH=$LIBSARUS_ROOT_PATH/spack/spack/bin:$PATH
-. $LIBSARUS_ROOT_PATH/spack/spack/share/spack/setup-env.sh
-spack env activate spack
+# WARNING: the local testing environment and GitLab CI/CD use Spack differently;
+# for local testing, dependencies are installed in
+# $LIBSARUS_ROOT_PATH/spack/spack but for GitLab CI/CD, dependencies are
+# installed in /opt/spack-environment. This subtle difference can make a
+# discrepancy that something works locally but not in CI/CD.
+if $($LOCAL_SPACK_PATH/spack env status | grep "No active env"); then 
+  export PATH=$LOCAL_SPACK_PATH:$PATH
+  . $LIBSARUS_ROOT_PATH/spack/spack/share/spack/setup-env.sh
+  spack env activate spack
+fi
 
 cmake -DCMAKE_TOOLCHAIN_FILE=$BUILD_DIR/../cmake/toolchain_files/$TOOLCHAIN_FILE \
       -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
