398fb853c97201fc8069ef809c2b5b8f38599fd5
Gwangmu Lee <gwangmu.lee@cscs.ch>
2024-08-08 17:42:01 +0200

    Removed auto-generated CTestTestfile.cmake

--- src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gwangmu/Projects/libsarus/src
-# Build directory: /home/gwangmu/Projects/libsarus/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
