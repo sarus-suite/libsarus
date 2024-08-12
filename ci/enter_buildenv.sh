08773dc2502f3eec598c5a1aec1b26765a72b776
Gwangmu Lee <gwangmu.lee@cscs.ch>
2024-08-12 17:04:46 +0200

    Fixed faulty path specification

--- ci/enter_buildenv.sh
+++ ci/enter_buildenv.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+CI_ROOT_PATH=$(dirname $(realpath ${BASH_SOURCE[0]}))
+
 CONTAINER_RT=docker
 OS_IMAGE="ubuntu:22.04"
 
@@ -55,7 +57,7 @@ if ! which $CONTAINER_RT >/dev/null 2>/dev/null; then
   exit 1
 fi
 
-if [[ -z $(dockerfile/containerize.sh -p | grep $OS_IMAGE) ]]; then
+if [[ -z $($CI_ROOT_PATH/dockerfile/containerize.sh -p | grep $OS_IMAGE) ]]; then
   error "os image '$OS_IMAGE' unsupported"
   exit 1
 fi
@@ -65,12 +67,12 @@ OS_IMAGE_NAME=libsarus/build/$OS_IMAGE
 # Create image if needed.
 # TODO: support other CONTAINER_RTs
 if [ -z "$(docker images -q $OS_IMAGE_NAME 2>/dev/null)" ]; then
-  pushd dockerfile
+  pushd $CI_ROOT_PATH/dockerfile
   ./containerize.sh $OS_IMAGE
   $CONTAINER_RT build -f Dockerfile.gen.$OS_IMAGE -t $OS_IMAGE_NAME .
   popd
 fi
 
 $CONTAINER_RT run --rm -it \
-  --mount type=bind,source=$PWD/..,target=/home/docker/libsarus \
+  --mount type=bind,source=$CI_ROOT_PATH/..,target=/home/docker/libsarus \
   --workdir /home/docker/libsarus $OS_IMAGE_NAME
