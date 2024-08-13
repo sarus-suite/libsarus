edbca74e4f9db486ff09bd972a82de2aa329d17a
Gwangmu Lee <gwangmu.lee@cscs.ch>
2024-08-13 17:29:31 +0200

    Revised ci/format_code.sh

--- ci/format_code.sh
+++ ci/format_code.sh
@@ -2,24 +2,15 @@
 
 LIBSARUS_ROOT_PATH=$(dirname $(realpath ${BASH_SOURCE[0]}))/..
 
-OPT_ALL_FILES=""
+OPT_FILE=""
 if [[ "$1" == "--all" ]]; then
-  OPT_ALL_FILES="--all-files"
-elif [[ -n "$1" ]]; then
+  OPT_FILES="--all-files"
+elif [[ -z "$1" ]]; then
+  OPT_FILES="--files $(git status --short | awk '{ print $2 }')"
+else
   echo "usage: $0 [--all]"
   echo "    --all     Format all source code (default: only Git-staged code)"
   exit 1
 fi
 
-PRE_COMMIT_INSTALLED=0
-if [[ -f $LIBSARUS_ROOT_PATH/.git/hooks/pre-commit ]]; then
-  PRE_COMMIT_INSTALLED=1
-fi
-
-if [[ $PRE_COMMIT_INSTALLED == 0 ]]; then
-  pre-commit install
-fi
-pre-commit run $OPT_ALL_FILES
-if [[ $PRE_COMMIT_INSTALLED == 0 ]]; then
-  pre-commit uninstall
-fi
+pre-commit run $OPT_FILES
