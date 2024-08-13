24aa598daa7fb0260ff62b0666b5921c5da3dbb0
Gwangmu Lee <gwangmu.lee@cscs.ch>
2024-08-13 16:58:20 +0200

    Revised ci/format_code.sh

--- ci/format_code.sh
+++ ci/format_code.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-# TODO: add '--all' option
+LIBSARUS_ROOT_PATH=$(dirname $(realpath ${BASH_SOURCE[0]}))/..
 
-ORG_CLANG_FORMAT=$(which clang-format)
-PATH=".:$PATH" pre-commit run --all-files
+OPT_ALL_FILES=""
+if [[ "$1" == "--all" ]]; then
+  OPT_ALL_FILES="--all-files"
+elif [[ -n "$1" ]]; then
+  echo "usage: $0 [--all]"
+  echo "    --all     Format all source code (default: only Git-staged code)"
+  exit 1
+fi
+
+PRE_COMMIT_INSTALLED=0
+if [[ -f $LIBSARUS_ROOT_PATH/.git/hooks/pre-commit ]]; then
+  PRE_COMMIT_INSTALLED=1
+fi
+
+if [[ $PRE_COMMIT_INSTALLED == 0 ]]; then
+  pre-commit install
+fi
+pre-commit run $OPT_ALL_FILES
+if [[ $PRE_COMMIT_INSTALLED == 0 ]]; then
+  pre-commit uninstall
+fi
