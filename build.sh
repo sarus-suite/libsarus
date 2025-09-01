3e52b88d8ed7c4ceb6ce09349ed29a66569baa57
Gwangmu Lee <gwangmu.lee@cscs.ch>
2025-09-01 12:12:03 +0200

    Testing Ubuntu 22.04 w/o Spack...

--- build.sh
+++ build.sh
@@ -1,39 +1,14 @@
 #!/bin/bash
 # Script: build.sh
 #
-# Build libsarus with the dependencies installed by Spack.
+# Build libsarus with the dependencies.
 
 LIBSARUS_ROOT_PATH=$(dirname $(realpath "${BASH_SOURCE[0]}"))
-LOCAL_SPACK_PATH=$LIBSARUS_ROOT_PATH/ci/spack/bin
 
 BUILD_DIR=$PWD/build
 INSTALL_DIR=$PWD/build/install
 TOOLCHAIN_FILE=gcc.cmake
 BUILD_TYPE=Debug
-
-# (Re)define variables if needed.
-for _ARG in "$@"; do
-  if [[ $_ARG == -D* ]]; then
-    _KEY=${_ARG#-D}; KEY=${_KEY%=*}
-    VALUE=${_ARG#*=}
-    echo "Setting $KEY to $VALUE..."
-    declare $KEY=$VALUE
-  fi
-done
-
-# WARNING: the local testing environment and GitLab CI/CD use Spack differently;
-# for local testing, dependencies are installed in
-# $LIBSARUS_ROOT_PATH/ci/spack but for GitLab CI/CD, dependencies are
-# installed in /home/docker/spack-environment. This subtle difference can make a
-# discrepancy that something works locally but not in CI/CD.
-if [[ $($LOCAL_SPACK_PATH/spack env status) == *"No active env"* ]]; then
-  export PATH=$LOCAL_SPACK_PATH:$PATH
-  . $LIBSARUS_ROOT_PATH/ci/spack/share/spack/setup-env.sh
-  if [ ! -f $LIBSARUS_ROOT_PATH/ci/spack.yaml ]; then
-    cp $LIBSARUS_ROOT_PATH/ci/spack.yaml.base $LIBSARUS_ROOT_PATH/ci/spack.yaml
-  fi
-  spack env activate ci
-fi
 
 cmake -DCMAKE_TOOLCHAIN_FILE=$BUILD_DIR/../cmake/toolchain_files/$TOOLCHAIN_FILE \
       -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
